f7f6290c95aedc6b3dff1a22538d2993303c5123
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 18:12:51 +0100

    removed build production from gitignore

---
+++ build/precache-manifest.b7525777cf0a1addb0fa2a0a7d875279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9905f5cc6dcf8049e423275890adfb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af4a3d17bb7687de7b9",
+    "url": "/static/css/main.58d9d140.chunk.css"
+  },
+  {
+    "revision": "0a739ec3ffcec37b1138",
+    "url": "/static/js/2.b53e5811.chunk.js"
+  },
+  {
+    "revision": "5af4a3d17bb7687de7b9",
+    "url": "/static/js/main.c8db4466.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
