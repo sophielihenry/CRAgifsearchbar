716cee5e65ade6ac4f90efd8acefb43ea280c796
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 17:48:07 +0100

    search bar now displays

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import SearchBar from './searchbar.js';
 
-import './App.css';
+import './App.scss';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <SearchBar search={this.search} />
+        <div className="container">
+          <div className="selectedGif">
+            <SearchBar search={this.search} />
+              <div className="selected-gif">
+
+              </div>
+          </div>
+          <div className="giflistContainer">
+
+          </div>
       </div>
+
       )
   }
 }
